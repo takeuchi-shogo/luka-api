bead04695a9858855b9238988bf69a447afad1c6
ShogoTakeuchi <smth.1.23@icloud.com>
2023-01-20 01:48:31 +0900

    small fix

--- app/src/interface/controllers/context.go
+++ app/src/interface/controllers/context.go
@@ -1,6 +1,8 @@
 package controllers
 
 type Context interface {
+	GetHeader(key string) string
+	Header(key, value string)
 	JSON(code int, obj interface{})
 	Param(key string) string
 	PostForm(key string) (value string)
--- app/src/interface/controllers/error_response.go
+++ app/src/interface/controllers/error_response.go
@@ -8,15 +8,6 @@ type ErrorResponse struct {
 	Error apierrors.ApiError `json:"error"`
 }
 
-type Error struct {
-	Next        error  `json:"error"`
-	StatusCode  int    `json:"code"`
-	Message     string `json:"message"`
-	InfoMessage string `json:"infoMessage"`
-	Type        string `json:"type"`
-	// Code        string `json:"errorCode"`
-}
-
 func NewErrorResponse(err apierrors.ApiError) *ErrorResponse {
 	res := new(ErrorResponse)
 	res.Error = err
